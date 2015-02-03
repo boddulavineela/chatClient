a2b876b60db2dc82f885e954826f4630f59edded
boddulavineela <vboddul@ncsu.edu>
2015-02-02 23:52:11 -0500

    Project 1 - Task 1,2,3

--- src/chat/client/gui/ChatActivity.java
+++ src/chat/client/gui/ChatActivity.java
@@ -48,6 +48,12 @@ import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import chat.client.agent.ChatClientInterface;
+//Task 3
+import android.location.Criteria; 
+import android.location.Location; 
+import android.location.LocationListener; 
+import android.location.LocationManager;
+import android.util.Log;
 
 /**
  * This activity implement the chat interface.
@@ -55,7 +61,15 @@ import chat.client.agent.ChatClientInterface;
  * @author Michele Izzo - Telecomitalia
  */
 
-public class ChatActivity extends Activity {
+public class ChatActivity extends Activity implements LocationListener{
+	
+	//Task 3
+	protected LocationManager locationManager; 
+	protected LocationListener locationListener; 
+	protected Context context; 
+	String mylocation;
+	
+	
 	private Logger logger = Logger.getJADELogger(this.getClass().getName());
 
 	static final int PARTICIPANTS_REQUEST = 0;
@@ -69,7 +83,11 @@ public class ChatActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Bundle extras = getIntent().getExtras();
+		//Task 3
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
+ 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+		
+ 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
 			nickname = extras.getString("nickname");
 		}
@@ -111,10 +129,24 @@ public class ChatActivity extends Activity {
 	private OnClickListener buttonSendListener = new OnClickListener() {
 		public void onClick(View v) {
 			final EditText messageField = (EditText) findViewById(R.id.edit_message);
+			
+			//Task 3
+			locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
+	 		Criteria criteria = new Criteria(); 
+	 		criteria.setAccuracy(Criteria.ACCURACY_FINE); 
+	 		criteria.setAltitudeRequired(false); 
+	 		criteria.setBearingRequired(false); 
+	 		criteria.setCostAllowed(true); 
+	 		criteria.setPowerRequirement(Criteria.POWER_LOW); 
+	 		
+	 		String provider = locationManager.getBestProvider(criteria, true); 
+	 		Location location = locationManager.getLastKnownLocation(provider); 
+	 		if (location != null) 
+	 		    	  mylocation = updateWithNewLocation(location); 
 			String message = messageField.getText().toString();
 			if (message != null && !message.equals("")) {
 				try {
-					chatClientInterface.handleSpoken(message);
+					chatClientInterface.handleSpoken(message+mylocation);
 					messageField.setText("");
 				} catch (O2AException e) {
 					showAlertDialog(e.getMessage(), false);
@@ -219,4 +251,72 @@ public class ChatActivity extends Activity {
 		AlertDialog alert = builder.create();
 		alert.show();		
 	}
+	
+	   @Override
+	    public void onLocationChanged(Location location) {
+	        //txtLat = (TextView) findViewById(R.id.chatTextView);
+	        //txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+	    }
+
+	    @Override
+	    public void onProviderDisabled(String provider) {
+	        Log.d("Latitude", "disable");
+	    }
+
+	    @Override
+	    public void onProviderEnabled(String provider) {
+	        Log.d("Latitude","enable");
+	    }
+
+	    @Override
+	    public void onStatusChanged(String provider, int status, Bundle extras) {
+	        Log.d("Latitude","status");
+	    }
+
+	    private String updateWithNewLocation(Location location) {
+	        //TextView myLocationText = (TextView) findViewById(R.id.myLocationText);
+
+	        String latLongString;
+
+	        if (location != null) {
+	            double lat = location.getLatitude();
+	            double lng = location.getLongitude();
+	            double loc1distance = getDistance(lat, lng, 35.77198, -78.67385);
+	            double loc2distance = getDistance(lat, lng, 38.90719, -77.03687);
+	            double loc3distance = getDistance(lat, lng, 48.85661, 2.35222);
+
+	            if(loc1distance < 50.00000)
+	            {
+	                latLongString = "\n\t@Latitude: " + lat + "\n\t\tLongitude: " + lng + "\n\t\t This is EBII (Centennial)";
+	            }
+	            else if (loc2distance < 50.00000)
+	            {
+	                latLongString = "\n\t@Latitude: " + lat + "\n\t\tLongitude: " + lng + "\n\t\t This is Washington DC";
+	            }
+	            else if (loc3distance < 50.00000)
+	            {
+	                latLongString = "\n\t@Latitude: " + lat + "\n\t\tLongitude: " + lng + "\n\t\t This is Paris";
+	            }
+	            else
+	            {
+	                latLongString = "\n\t@Latitude: " + lat + "\n\t\tLongitude: " + lng ;
+	            }
+	            }
+	        else {
+	            latLongString = "No location found";
+	        }
+
+	        return latLongString;
+	    }
+
+	    private static double getDistance(double lat1, double lon1, double lat2, double lon2) {
+	        final double Radius = 6371 * 1E3; //Earth's mean radius
+	        double dLat = Math.toRadians(lat2-lat1);
+	        double dLon = Math.toRadians(lon2-lon1);
+	        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+	                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+	                        Math.sin(dLon/2) * Math.sin(dLon/2);
+	        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	        return Radius * c;
+	    }
 }
